a8726809f1b5f2b9a63026aa178c301a30b289ff
sumandari <sumandari.ee@gmail.com>
2020-08-02 21:44:27 +0800

    Ass unit test for calculator

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+"""
+Unit test for the calculator library
+"""
+
+import calculator
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_substraction(self):
+        assert 3 == calculator.subtract(6, 3)
