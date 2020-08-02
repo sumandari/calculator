64be4ce95a7d071f781ed12733a2073c871747a0
sumandari <sumandari.ee@gmail.com>
2020-08-02 22:09:45 +0800

    add multiply test

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,7 @@ class TestCalculator:
 
     def test_substraction(self):
         assert 3 == calculator.subtract(6, 3)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
+
